2109e9c6acefbaa9acf2a23d930787190007daaa
Kakekzeus12 <148560685+Kakekzeus12@users.noreply.github.com>
2025-01-20 02:47:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ except ValueError:
 # Pesan Saat Memaksa Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu\n\n JOIN RIBUAN VIDEO MURAH\nhttps://t.me/TESTIVIPROOM69/12</b>",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu\n\n Nonton video tanpa link,murah,dan terupdate Join VVIP=>>\nhttps://t.me/VIP_MURAH_MERIAH/3</b>",
 )
 
 # Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
